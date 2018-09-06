05271844d6438ba9b097082f4f88be97e7d32b6c
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-09-06 18:37:25 +0600

    Tyr To recieve sms

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,3 +1,13 @@
+char RcvdMsg[200] = " ";
+int RcvdCheck = 0;
+int RcvdConf = 0;
+int index = 0;
+int RcvdEnd = 0;
+char MsgMob[15];
+char MsgTxt[50] = " ";
+int MsgLength = 0;
+
+
 // FOR Liquid Cristal Display AND EEPROM
 
 #include <EEPROM.h>
@@ -13,30 +23,24 @@ float unit = 0.0;
 int totalPulse = 0;
 
 // FOR SIM800l MODULE
-#include <Sim800l.h>
 #include <SoftwareSerial.h>
-Sim800l Sim800l;
-char* text;
-char* number;
-
+SoftwareSerial cell(10, 11);
+char incoming_char = 0;
 
 void setup() {
   Serial.begin(9600);
-  Sim800l.begin();
-  text="Testing Sms";
-  number="+8801000000000";
-//  Sim800l.sendSms(number,text);
+  cell.begin(9600);
 }
 
 void loop() {
   // Re-Store The Previous Unit Value AND Pulse Count
   unit = EEPROM.read(unitAddress);
   EEPROM.write(unitAddress, unit);
-  
+
   totalPulse = EEPROM.read(totalPulseAddress);
   EEPROM.write(totalPulseAddress, totalPulse);
   // 8ms Occure For Read And Write Data
-  
+
   // Take the sensor reading and count pulse
   if (analogRead(A1) > 200)totalPulse = totalPulse + 1;
   delay(72);
@@ -56,6 +60,72 @@ void loop() {
 
   delay(500);
 
-  
 
+  // Delete All SMS IN PHONE
+  if (cell.available() > 0 && RecSMS()) Serial.println("*** Success ***"); // Send Unit Value To Server
+  else Serial.println("-- Nothing -- ");
+
+}
+
+boolean RecSMS() {
+  boolean flag = false;
+
+  if (cell.available())
+  {
+    char data = cell.read();
+    if (data == '+') {
+      RcvdCheck = 1;
+    }
+    if ((data == 'C') && (RcvdCheck == 1)) {
+      RcvdCheck = 2;
+    }
+    if ((data == 'M') && (RcvdCheck == 2)) {
+      RcvdCheck = 3;
+    }
+    if ((data == 'T') && (RcvdCheck == 3)) {
+      RcvdCheck = 4;
+    }
+    if (RcvdCheck == 4) {
+      index = 0;
+      RcvdConf = 1;
+      RcvdCheck = 0;
+      flag = true;
+    }
+    if (RcvdConf == 1)
+    {
+      if (data == '\n') {
+        RcvdEnd++;
+      }
+      if (RcvdEnd == 3) {
+        RcvdEnd = 0;
+      }
+      RcvdMsg[index] = data;
+
+      index++;
+      if (RcvdEnd == 2) {
+        RcvdConf = 0;
+        MsgLength = index - 2;
+      }
+      if (RcvdConf == 0)
+      {
+        Serial.print("Mobile Number is: ");
+        for (int x = 4; x < 17; x++)
+        {
+          MsgMob[x - 4] = RcvdMsg[x];
+          Serial.print(MsgMob[x - 4]);
+        }
+        Serial.println();
+        Serial.print("Message Text: ");
+        for (int x = 54; x < MsgLength; x++)
+        {
+          MsgTxt[x - 54] = RcvdMsg[x];
+          Serial.print(MsgTxt[x - 54]);
+        }
+
+        Serial.println();
+      }
+    }
+  }
+
+  return flag;
 }
