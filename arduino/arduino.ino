3beb6b55f2ff877cb6a7c1e809c3b037919ad48c
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-09-06 17:46:03 +0600

    Show Data In Serial And LCD

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,8 +2,15 @@
 
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
+
 const int rs = 8, en = 7, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int unitAddress = 0; // EEPROM ADDRESS (Arduino Uno -> 1kb EEPROM storage.)
+int totalPulseAddress = 1;
+
+float unit = 0.0;
+int totalPulse = 0;
 
 // FOR SIM800l MODULE
 #include <Sim800l.h>
@@ -14,13 +21,41 @@ char* number;
 
 
 void setup() {
+  Serial.begin(9600);
   Sim800l.begin();
   text="Testing Sms";
   number="+8801000000000";
-  Sim800l.sendSms(number,text);
+//  Sim800l.sendSms(number,text);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Re-Store The Previous Unit Value AND Pulse Count
+  unit = EEPROM.read(unitAddress);
+  EEPROM.write(unitAddress, unit);
+  
+  totalPulse = EEPROM.read(totalPulseAddress);
+  EEPROM.write(totalPulseAddress, totalPulse);
+  // 8ms Occure For Read And Write Data
+  
+  // Take the sensor reading and count pulse
+  if (analogRead(A1) > 200)totalPulse = totalPulse + 1;
+  delay(72);
+
+  // Calculate The Unit Value
+  unit = totalPulse / 16;
+
+  // Display In Serial Monitor
+  Serial.println("Total Pulse : " + String(totalPulse) + " Unit: " + String(unit));
+
+  // Display In LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Pulse:" + String(totalPulse));
+  lcd.setCursor(16, 1);
+  lcd.print("Unit:" + String(unit));
+
+  delay(500);
+
+  
 
 }
